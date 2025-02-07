318bb44cee99821dfdfb722a0bd7bb4ad4c65dc5
JurgenMeti <jurgenmeti@gmail.com>
2025-02-07 16:42:07 +0100

    hero section 1/3

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,23 @@
+import { Section } from "lucide-react"
+import SearchForm from "../components/SearchForm";
 
 
-export default function Home() {
+export default async function Home({searchParams}:{
+  searchParams: Promise <{query?:string}>
+}) {
+  const query=(await searchParams).query;
+
   return (
-    <h1 className="text-2xl">Home</h1>
+    <section className="container">
+    <h1 className="heading">
+      Home<br />
+      Connect</h1>
+
+      <p className="sub-heading !max-w-3xl">
+       Share ideas 
+      </p>
+
+      <SearchForm query={query}/>
+    </section>
   );
 }
